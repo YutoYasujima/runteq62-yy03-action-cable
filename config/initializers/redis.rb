bfc01b65e77bbadcf5fd759161c42fef99dd1b6a
YutoYasujima <apyfm.3.25.y.y@gmail.com>
2025-03-29 15:12:30 +0900

    fixed:Redisサーバーへの接続不良修正2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 require "redis"
 
 $Redis = Redis.new(
-  url: ENV["REDIS_URL"],
-  password: ENV["REDIS_PASSWORD"]
-  )
+  url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0",
+  timeout: 5 # 必要に応じてタイムアウト時間を設定
+)
